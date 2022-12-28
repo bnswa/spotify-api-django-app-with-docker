31de7d746d3f59b27fd32d5683ef816d63734a7f
briannaswan <76968876+briannaswan@users.noreply.github.com>
2022-12-27 20:06:46 -0800

    Create credentials.py
    Requires inputs. See comments.

---
+++ credentials.py
@@ -0,0 +1,6 @@
+client_ID=''       # Fill in with the value from the Spotify Developer Dashboard
+client_SECRET=''   # Fill in with the value from the Spotify Developer Dashboard
+redirect_URI="http://localhost:[PORT]]/music/callback"  # Fill in with the port you want to use -- MUST be one of the 
+                                                                   # redirect urls as in Spotify Developer Dashboard 
+username=''        # Fill in with Spotify username you want to use                                                                                                      
+scope = "user-read-private playlist-read-private playlist-modify-public user-top-read"  # Set the scope(s)
